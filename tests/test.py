532ca854e38543ecef265eb4e87547cfd47c7287
guy <guy.simons@live.nl>
2020-03-11 16:17:57 +0100

    Some new thing

--- tests/test.py
+++ tests/test.py
@@ -6,3 +6,10 @@ class TestProbleem(unittest.TestCase):
         h=1
         self.assertEqual(h,1)
 
+if __name__ == '__main__':
+    ############# Add these lines #############
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    ###########################################
+    unittest.main()
